f9fdddc05c190ddd113d9e71417c45aadba37a76
Axel Fahy <axel@fahy.net>
2024-06-14 22:09:02 +0200

    fix(versioneer): update versioneer for python 3.12

--- rhinopics/__init__.py
+++ rhinopics/__init__.py
@@ -9,3 +9,6 @@ logging.basicConfig(format=FORMAT, level=logging.ERROR)
 
 __version__ = get_versions()['version']
 del get_versions
+
+from . import _version
+__version__ = _version.get_versions()['version']
